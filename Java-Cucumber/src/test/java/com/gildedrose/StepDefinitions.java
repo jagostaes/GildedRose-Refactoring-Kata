79fa90a2c74456da75fdc70c2edb3652896efd18
jstaes <jago.staes@gmail.com>
2025-06-09 20:06:40 +0200

    (test): fix existing bogus gherkin scenario

--- Java-Cucumber/src/test/java/com/gildedrose/StepDefinitions.java
+++ Java-Cucumber/src/test/java/com/gildedrose/StepDefinitions.java
@@ -1,18 +1,18 @@
 package com.gildedrose;
 
-import static org.junit.Assert.*;
-
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class StepDefinitions {
     private Item[] items = new Item[1];
     private GildedRose app;
 
-    @Given("The item as {string}")
-    public void initial_sellin_is_and_quality_is(String name) {
-        items[0] = new Item(name, 0, 0);
+    @Given("The item as {string} with quality {int}")
+    public void initial_sellin_is_and_quality_is(String name, int quality) {
+        items[0] = new Item(name, 0, quality);
         app = new GildedRose(items);
     }
 
@@ -23,7 +23,12 @@ public class StepDefinitions {
 
     @Then("I should get item as {string}")
     public void i_should_get_sellin_as_and_quality_as(String expected) {
-        assertEquals(expected, app.items[0].name);
+        assertThat(expected).isEqualTo(app.items[0].name);
+    }
+
+    @Then("The quality is updated to {int}")
+    public void the_quality_should_be_updated(int expected) {
+        assertThat(expected).isEqualTo(app.items[0].quality);
     }
 }
 
--- Java-Cucumber/src/test/resources/com/gildedrose/GildedRose.feature
+++ Java-Cucumber/src/test/resources/com/gildedrose/GildedRose.feature
@@ -1,7 +1,8 @@
 Feature: Gilded Rose quality
   I want to know if the quality is updated properly
 
-  Scenario: Checking foo
-    Given The item as "fixme"
+  Scenario: Checking if quality is updated
+    Given The item as "foo" with quality 10
     When I update the quality
-    Then I should get item as "foo"
+    Then The quality is updated to 8
+
