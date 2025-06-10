4297fd7b0ad78db8267b532f63d1240f73fd8308
jstaes <jago.staes@gmail.com>
2025-06-10 18:13:06 +0200

    (refactor): extract backstage passes logic from updateQuality method

--- Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
+++ Java-Cucumber/src/main/java/com/gildedrose/GildedRose.java
@@ -4,6 +4,8 @@ import com.gildedrose.domain.Item;
 
 import java.util.List;
 
+import static java.lang.Integer.valueOf;
+
 class GildedRose {
     List<Item> items;
 
@@ -19,24 +21,10 @@ class GildedRose {
 
             if (isRegularItem(item) && item.quality > 0) {
                 decreaseQuality(item, 1);
+            } else if (isBackstagePasses(item)) {
+                updateQualityOfBackstagePasses(item);
             } else {
-                if (item.quality < 50) {
-                    increaseQuality(item, 1);
-
-                    if (isBackstagePasses(item)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                increaseQuality(item, 1);
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                increaseQuality(item, 1);
-                            }
-                        }
-                    }
-                }
+                increaseQuality(item, 1);
             }
 
 
@@ -60,6 +48,15 @@ class GildedRose {
         }
     }
 
+    private static void updateQualityOfBackstagePasses(Item item) {
+        switch (valueOf(item.sellIn)) {
+            case Integer s when s >= 10 -> increaseQuality(item, 1);
+            case Integer s when s >= 5 -> increaseQuality(item, 2);
+            case Integer s when s >= 0 -> increaseQuality(item, 3);
+            default -> resetQuality(item);
+        }
+    }
+
     private static void resetQuality(Item item) {
         item.quality = 0;
     }
@@ -69,7 +66,9 @@ class GildedRose {
     }
 
     private static void increaseQuality(Item item, int amount) {
-        item.quality = item.quality + amount;
+        if (!isSulfuras(item)) {
+            item.quality = Math.min((item.quality + amount), 50);
+        }
     }
 
     private static void decreaseSellIn(Item item) {
